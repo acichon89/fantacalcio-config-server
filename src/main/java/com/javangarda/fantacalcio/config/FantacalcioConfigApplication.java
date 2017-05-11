aa40f382e64711f10c15b7006dcbb872defe118a
acichon <acichon89@gmail.com>
2017-05-11 23:40:10 +0200

    initial commit

---
+++ src/main/java/com/javangarda/fantacalcio/config/FantacalcioConfigApplication.java
@@ -0,0 +1,14 @@
+package com.javangarda.fantacalcio.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class FantacalcioConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantacalcioConfigApplication.class, args);
+	}
+}
